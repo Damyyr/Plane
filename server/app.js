6c3f20266b501eb88a6860c574922135e636de99
Damyyr <maxime.damour974@gmail.com>
2019-03-09 18:15:20 -0500

    return data

--- server/app.js
+++ server/app.js
@@ -38,6 +38,79 @@ let intersectSchema = mongoose.Schema({
 
 let IntersectModel = mongoose.model('IntersectModel', intersectSchema);
 
+function handleError(error){
+  console.log(error);
+}
+
+io.on("connection", client => {
+
+  console.log("Sup bitch");
+  
+  client.on('lightStates', data => {
+    let response = []
+    IntersectModel.find({'Int_no': data.data[0]}, function (err, res) {
+      if (err) return handleError(err);
+
+      for (const intersection of res) {
+        let lastChange = intersection.lastChange;
+        let secondsSinceLastChange = Math.round((new Date - lastChange) / 1000);
+        let dirA = intersection.directions.filter(elem => elem.direction = 'A')[0];
+        let dirB = intersection.directions.filter(elem => elem.direction = 'B')[0];
+
+        let totalCycle = dirA.directionTimer + dirB.directionTimer;
+        let direction = secondsSinceLastChange % totalCycle;
+        let greenFor = direction <= dirA.directionTimer ? 'A' : 'B';
+
+        response.push({
+          Int_no: intersection.Int_no,
+          greenFor: greenFor
+        });
+      }
+
+      console.log(response);
+    });
+
+    client.emit('lightStates', { data: response })
+  });
+
+  client.on("feedback", data => { 
+    IntersectModel.find({ 'Int_no': 661 }, 'lat long', function (err, mintersect) {
+      if (err) return handleError(err);
+
+      let url = `https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?point=${mintersect[0].lat}%2C${mintersect[0].long}&unit=KMPH&key=${process.env.tomtomapi}`
+      // let url = `https://traffic.api.here.com/traffic/6.1/flow.json?bbox=${mintersect[0].lat}%2C${mintersect[0].long}%3B${mintersect[0].lat}%2C${mintersect[0].long}&app_id=${process.env.hereappid}&app_code=${process.env.hereappcode}`
+      axios.get(url).then((resp) => {
+        console.log(resp.data.flowSegmentData);
+        if (resp.data.flowSegmentData) {
+          client.emit("feedback-answer", { data: `Retour de l'algo vraiment fou ${algoVraimentComplique(resp.data.flowSegmentData)} || ${mintersect[0].lat}, ${mintersect[0].long}` })
+        }
+      }).catch((err) => {
+        throw err
+      })
+    })
+  })
+
+  client.on("disconnect", () => { 
+    console.log("bye")
+  })
+})
+
+function algoVraimentComplique(flowData) {
+  let a = flowData.currentSpeed
+  let b = flowData.freeFlowSpeed
+  let ab = a/b;
+  return `${a} || ${b} || ${ab}`;
+}
+
+server.listen(PORT, hostname, () => {
+    console.log("Started")
+  
+    console.log(`Server running at http://${hostname}:${PORT}/`);
+  
+});
+
+// ------------------------------------------------------- Migration de pauvre
+
 // for (const i of intersect) {
     
 //   let dirA = {
@@ -95,70 +168,3 @@ let IntersectModel = mongoose.model('IntersectModel', intersectSchema);
 //     console.log('UserSchema successfully saved.');
 //   });
 // }
-
-io.on("connection", client => {
-
-  console.log("Sup bitch");
-  
-  client.on('lightStates', data => {
-    let response = []
-    IntersectModel.find({'Int_no': {$in:data}}, function (err, res) {
-      if (err) return handleError(err);
-
-      for (const intersection of res) {
-        let lastChange = res.lastChange;
-        let secondsSinceLastChange = Math.round((Date.new - lastChange) / 1000);
-        let dirA = intersection.directions.filter(elem => elem.direction = 'A')[0];
-        let dirB = intersection.directions.filter(elem => elem.direction = 'B')[0];
-
-        let totalCycle = dirA.directionTimer + dirB.directionTimer;
-        let direction = secondsSinceLastChange % totalCycle;
-        let greenFor = direction <= dirA.directionTimer ? 'A' : 'B';
-
-        response.push({
-          Int_no: res.Int_no,
-          greenFor: greenFor
-        });
-      }
-
-      console.log(response);
-    });
-
-    client.emit('lightStates', { data: response })
-  });
-
-  client.on("feedback", data => { 
-    IntersectModel.find({ 'Int_no': 661 }, 'lat long', function (err, mintersect) {
-      if (err) return handleError(err);
-
-      let url = `https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?point=${mintersect[0].lat}%2C${mintersect[0].long}&unit=KMPH&key=${process.env.tomtomapi}`
-      // let url = `https://traffic.api.here.com/traffic/6.1/flow.json?bbox=${mintersect[0].lat}%2C${mintersect[0].long}%3B${mintersect[0].lat}%2C${mintersect[0].long}&app_id=${process.env.hereappid}&app_code=${process.env.hereappcode}`
-      axios.get(url).then((resp) => {
-        console.log(resp.data.flowSegmentData);
-        if (resp.data.flowSegmentData) {
-          client.emit("feedback-answer", { data: `Retour de l'algo vraiment fou ${algoVraimentComplique(resp.data.flowSegmentData)} || ${mintersect[0].lat}, ${mintersect[0].long}` })
-        }
-      }).catch((err) => {
-        throw err
-      })
-    })
-  })
-
-  client.on("disconnect", () => { 
-    console.log("bye")
-  })
-})
-
-function algoVraimentComplique(flowData) {
-  let a = flowData.currentSpeed
-  let b = flowData.freeFlowSpeed
-  let ab = a/b;
-  return `${a} || ${b} || ${ab}`;
-}
-
-server.listen(PORT, hostname, () => {
-    console.log("Started")
-  
-    console.log(`Server running at http://${hostname}:${PORT}/`);
-  
-  });
